753f6c1d2f80bc91d4e1b870fc4e12ba1e7f1132
Pranav Bharadwaj <78376336+Pranav1723@users.noreply.github.com>
2023-10-04 11:35:54 +0530

    Update and rename sign_unsign.c to signed_unsigned.c

---
+++ DAY_1/signed_unsigned.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+	unsigned long long int max = (unsigned long long int) (pow(2,64) -1);
+	printf("Largest unsigned long long int = %llu/n", max);
+	return 0;
+}
